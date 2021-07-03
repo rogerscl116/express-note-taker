c261de851296444543d75302be370c5e121b562f
charity.rogers <rogerscl116@gmail.com>
2021-07-03 14:09:32 -0500

    add delete route

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -18,4 +18,16 @@ router.post('/api/notes', (req, res) => {
     res.json(newNote);
   });
 
+// // gets a note with specific id
+// router.get("/api/notes/:id", (req,res) => {
+//     res.json(notes[req.params.id]);
+// });
+
+// // deletes a note with specific id
+// router.delete("/api/notes/:id", (req, res) => {
+//     notes.splice(req.params.id, 1);
+//     updateDb();
+//     console.log("Deleted note with id "+req.params.id);
+// });
+
 module.exports = router;
