701e5fac169d66b7010dc65d62bd5fcd2bca7e78
charity.rogers <rogerscl116@gmail.com>
2021-07-04 15:38:38 -0500

    add keep.js with class

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
-// require express package
+// dependencies
 const express = require('express');
-
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+
 // parse incoming JSON data
 app.use(express.json());
+
 app.use(express.static('public'));
 
-// use apiRoutes
+
+// use api and htmlRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
